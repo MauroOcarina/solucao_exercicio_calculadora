afd6c6e1ddf153d119d0976025faf8ac4a8ccb07
Mauro de Melo <Mauro de Melo@DESKTOP-4DQFCLS>
2020-02-25 20:06:33 -0300

    implementando a Soma

--- src/exercicio_calculadora_git/Calculadora.java
+++ src/exercicio_calculadora_git/Calculadora.java
@@ -1,5 +1,32 @@
 package exercicio_calculadora_git;
 
-public class Calculadora {
+import java.util.*;
 
+public class Calculadora {
+	static double resultado;
+
+	public static void main(String[] args) {
+
+		Scanner calc = new Scanner(System.in);
+		System.out.println("Digite o número 01: ");
+		double num1 = calc.nextDouble();
+		System.out.println("Digite o número 02: ");
+		double num2 = calc.nextDouble();
+		System.out.print(
+				"Escolha uma das opções: \n 01: Somar \n 02: Subtrair \n 03: Multiplicar  \n 04: Dividir: \n Opção escolhida: ");
+		int num3 = calc.nextInt();
+		if (num3 < 5 && num3 > 0) {
+			switch (num3) {
+
+			case 1:
+				resultado = num1 + num2;
+				break;
+
+			}
+			System.out.println("Resultado igual a: " + resultado);
+		} else {
+			System.out.println("Reinicie o programa e insira um número entre 1 a 4. \nObrigado pela compreensão");
+		}
+
+	}
 }
